a48393140064438c8fa81adbdcb082749aba530d
grondag <grondag@gmail.com>
2019-09-11 17:07:38 -0700

    Wedge caps

---
+++ src/main/java/grondag/xm/api/block/base/NonCubicFacingBlock.java
@@ -0,0 +1,78 @@
+/*******************************************************************************
+ * Copyright 2019 grondag
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.  You may obtain a copy
+ * of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ ******************************************************************************/
+
+package grondag.xm.api.block.base;
+
+import static org.apiguardian.api.API.Status.EXPERIMENTAL;
+
+import org.apiguardian.api.API;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.FacingBlock;
+import net.minecraft.fluid.FluidState;
+import net.minecraft.fluid.Fluids;
+import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.state.StateFactory.Builder;
+import net.minecraft.state.property.Properties;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.world.IWorld;
+
+/**
+ * Handles waterlogging.
+ */
+@API(status = EXPERIMENTAL)
+public class NonCubicFacingBlock extends FacingBlock {
+    
+    public NonCubicFacingBlock(Settings settings, Direction defaultFace) {
+        super(settings);
+        setDefaultState(getDefaultState().with(FACING, defaultFace));
+    }
+
+    @Override
+    public BlockState getPlacementState(ItemPlacementContext ctx) {
+        FluidState fluidState = ctx.getWorld().getFluidState(ctx.getBlockPos());
+        return this.getDefaultState()
+                .with(FACING, ctx.getSide().getOpposite())
+                .with(Properties.WATERLOGGED, fluidState.getFluid() == Fluids.WATER);
+     }
+    
+    @Override
+    protected void appendProperties(Builder<Block, BlockState> builder) {
+        super.appendProperties(builder);
+        builder.add(FACING);
+        builder.add(Properties.WATERLOGGED);
+    }
+    
+    @Override
+    public boolean hasSidedTransparency(BlockState blockState_1) {
+        return true;
+    }
+    
+    @Override
+    public BlockState getStateForNeighborUpdate(BlockState blockState, Direction face, BlockState blockState2, IWorld world, BlockPos pos, BlockPos pos2) {
+        if (blockState.get(Properties.WATERLOGGED)) {
+           world.getFluidTickScheduler().schedule(pos, Fluids.WATER, Fluids.WATER.getTickRate(world));
+        }
+        return super.getStateForNeighborUpdate(blockState, face, blockState2, world, pos, pos2);
+     }
+    
+    @Override
+    public FluidState getFluidState(BlockState blockState) {
+        return blockState.get(Properties.WATERLOGGED) ? Fluids.WATER.getStill(false) : super.getFluidState(blockState);
+     }
+}
---
+++ src/main/java/grondag/xm/api/primitive/simple/WedgeCap.java
@@ -0,0 +1,111 @@
+/*******************************************************************************
+ * Copyright 2019 grondag
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.  You may obtain a copy
+ * of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ ******************************************************************************/
+package grondag.xm.api.primitive.simple;
+
+import java.util.function.Function;
+
+import grondag.xm.Xm;
+import grondag.xm.api.mesh.WritableMesh;
+import grondag.xm.api.mesh.XmMesh;
+import grondag.xm.api.mesh.XmMeshes;
+import grondag.xm.api.mesh.polygon.MutablePolygon;
+import grondag.xm.api.mesh.polygon.PolyTransform;
+import grondag.xm.api.modelstate.primitive.PrimitiveState;
+import grondag.xm.api.orientation.OrientationType;
+import grondag.xm.api.paint.SurfaceTopology;
+import grondag.xm.api.primitive.SimplePrimitive;
+import grondag.xm.api.primitive.surface.XmSurface;
+import grondag.xm.api.primitive.surface.XmSurfaceList;
+import grondag.xm.api.texture.TextureOrientation;
+import net.minecraft.util.math.Direction;
+
+public class WedgeCap  {
+    public static final XmSurfaceList SURFACES = XmSurfaceList.builder()
+            .add("bottom", SurfaceTopology.CUBIC, XmSurface.FLAG_ALLOW_BORDERS)
+            .add("top", SurfaceTopology.CUBIC, XmSurface.FLAG_ALLOW_BORDERS)
+            .build();
+
+    public static final XmSurface SURFACE_BOTTOM = SURFACES.get(0);
+    public static final XmSurface SURFACE_TOP = SURFACES.get(1);
+    
+    static final Function<PrimitiveState, XmMesh> POLY_FACTORY = modelState -> {
+        final PolyTransform transform = PolyTransform.get(modelState);
+        
+        WritableMesh mesh = XmMeshes.claimWritable();
+        MutablePolygon writer = mesh.writer()
+            .colorAll(0, 0xFFFFFFFF)
+            .lockUV(0, true)
+            .rotation(0, TextureOrientation.IDENTITY)
+            .sprite(0, "")
+            .saveDefaults();
+            
+        writer
+            .surface(SURFACE_BOTTOM)
+            .setupFaceQuad(Direction.DOWN, 0, 0, 1, 1, 0, Direction.NORTH)
+            .apply(transform)
+            .append();
+        
+        writer
+            .surface(SURFACE_TOP)
+            .vertexCount(3)
+            .pos(0, 0, 0, 0).color(0, 0, 0xFFFFFFFF)
+            .pos(1, 0.5f, 0.5f, 0.5f).color(1, 0, 0xFFFFFFFF)
+            .pos(2, 1, 0, 0).color(2, 0, 0xFFFFFFFF)
+            .nominalFace(Direction.UP)
+            .apply(transform)
+            .append();
+
+        writer
+            .surface(SURFACE_TOP)
+            .vertexCount(3)
+            .pos(0, 1, 0, 0).color(0, 0, 0xFFFFFFFF)
+            .pos(1, 0.5f, 0.5f, 0.5f).color(1, 0, 0xFFFFFFFF)
+            .pos(2, 1, 0, 1).color(2, 0, 0xFFFFFFFF)
+            .nominalFace(Direction.UP)
+            .apply(transform)
+            .append();
+
+        writer
+            .surface(SURFACE_TOP)
+            .vertexCount(3)
+            .pos(0, 1, 0, 1).color(0, 0, 0xFFFFFFFF)
+            .pos(1, 0.5f, 0.5f, 0.5f).color(1, 0, 0xFFFFFFFF)
+            .pos(2, 0, 0, 1).color(2, 0, 0xFFFFFFFF)
+            .nominalFace(Direction.UP)
+            .apply(transform)
+            .append();
+        
+        writer
+            .surface(SURFACE_TOP)
+            .vertexCount(3)
+            .pos(0, 0, 0, 1).color(0, 0, 0xFFFFFFFF)
+            .pos(1, 0.5f, 0.5f, 0.5f).color(1, 0, 0xFFFFFFFF)
+            .pos(2, 0, 0, 0).color(2, 0, 0xFFFFFFFF)
+            .nominalFace(Direction.UP)
+            .apply(transform)
+            .append();
+        
+        return mesh.releaseToReader();
+    };
+    
+
+    public static final SimplePrimitive INSTANCE = SimplePrimitive.builder()
+            .surfaceList(SURFACES)
+            .polyFactory(POLY_FACTORY)
+            .orientationType(OrientationType.FACE)
+            .build(Xm.idString("wedge_cap"));
+    
+}
