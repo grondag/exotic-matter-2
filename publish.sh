4c1fcc554a9b18c79941cc5dca627cffb73e0252
Grondag <grondag@gmail.com>
2020-05-28 07:40:08 -0700

    Update publish.sh

--- publish.sh
+++ publish.sh
@@ -3,6 +3,7 @@ echo ====================================
 echo "Exotic Matter"
 echo ====================================
 ./gradlew publish
+./gradlew build --rerun-tasks
 
 cd builds
 
@@ -11,6 +12,7 @@ echo "Exotic Art Core"
 echo ====================================
 cd core
 ../../gradlew publish
+../../gradlew build --rerun-tasks
 cd ..
 
 echo ====================================
@@ -18,6 +20,7 @@ echo "Exotic Art Test"
 echo ====================================
 cd test
 ../../gradlew publish
+../../gradlew build --rerun-tasks
 cd ..
 
 echo ====================================
@@ -25,6 +28,7 @@ echo "Exotic Art Unstable"
 echo ====================================
 cd unstable
 ../../gradlew publish
+../../gradlew build --rerun-tasks
 cd ..
 
 cd ..
