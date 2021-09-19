8e2ba239192021332323edc2df6b02497dce4245
grondag <>
2021-09-19 15:41:09 -0700

    Repair mojmap damage, fix Frex deps

--- src/main/java/grondag/xm/mixin/client/MixinTerrainParticle.java
+++ src/main/java/grondag/xm/mixin/client/MixinTerrainParticle.java
@@ -19,24 +19,26 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import grondag.xm.api.block.XmBlockState;
-import grondag.xm.api.modelstate.ModelState;
-import grondag.xm.api.modelstate.MutableModelState;
-import grondag.xm.dispatch.XmDispatcher;
+
 import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.particle.TerrainParticle;
 import net.minecraft.client.particle.TextureSheetParticle;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.block.state.BlockState;
 
+import grondag.xm.api.block.XmBlockState;
+import grondag.xm.api.modelstate.ModelState;
+import grondag.xm.api.modelstate.MutableModelState;
+import grondag.xm.dispatch.XmDispatcher;
+
 @Mixin(TerrainParticle.class)
-public abstract class MixinBlockDustParticle extends TextureSheetParticle {
+public abstract class MixinTerrainParticle extends TextureSheetParticle {
 	// not used
-	protected MixinBlockDustParticle(ClientLevel world_1, double double_1, double double_2, double double_3) {
+	protected MixinTerrainParticle(ClientLevel world_1, double double_1, double double_2, double double_3) {
 		super(world_1, double_1, double_2, double_3);
 	}
 
-	@Inject(method = "<init>(Lnet/minecraft/client/world/ClientWorld;DDDDDDLnet/minecraft/block/BlockState;Lnet/minecraft/util/math/BlockPos;)V", at = @At(value = "RETURN"), cancellable = false, require = 0)
+	@Inject(method = "<init>(Lnet/minecraft/client/multiplayer/ClientLevel;DDDDDDLnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/core/BlockPos;)V", at = @At(value = "RETURN"), cancellable = false, require = 0)
 	void onNew(ClientLevel world, double x, double y, double z, double velocityX, double velocityY, double velocityZ, BlockState blockState, BlockPos blockPos, CallbackInfo ci) {
 		final MutableModelState lookupState = XmBlockState.modelState(blockState, world, blockPos, false);
 
